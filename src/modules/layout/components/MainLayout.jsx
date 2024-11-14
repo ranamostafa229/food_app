5a1e3a1928d41047b86903cfe5a15df6e68ed205
Your Name <ranamostafa2210@gmail.com>
2024-11-14 20:43:35 +0200

    fix sidebar

--- src/modules/layout/components/MainLayout.jsx
+++ src/modules/layout/components/MainLayout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../shared/components/Navbar/Navbar";
-import SideBar from "../../shared/components/SideBarMenu/SideBar";
+import SideBarMenu from "../../shared/components/SideBarMenu/SideBar";
 
 // eslint-disable-next-line react/prop-types
 const MainLayout = ({ loginData, removeLoginData }) => {
   return (
     <div className="d-flex ">
-      <SideBar removeLoginData={removeLoginData} />
+      <SideBarMenu removeLoginData={removeLoginData} />
       <div className="col-9 col-md-11 mx-auto">
         <Navbar loginData={loginData} />
         <Outlet />
