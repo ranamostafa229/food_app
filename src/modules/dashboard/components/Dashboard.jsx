76c3a3bf7cfd08c9d2f8f11a8fbdd8f750e93de8
Your Name <ranamostafa2210@gmail.com>
2024-11-22 00:25:29 +0200

    add fill recipes header

--- src/modules/dashboard/components/Dashboard.jsx
+++ src/modules/dashboard/components/Dashboard.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable react/prop-types */
+import FillRecipesHeader from "../../shared/components/FillRecipesHeader/FillRecipesHeader";
 import Header from "../../shared/components/Header/Header";
 
 const Home = ({ loginData }) => {
   return (
-    <div>
+    <div className="d-flex flex-column gap-3">
       <Header
         title={`Welcome ${loginData?.userName} `}
         description={
           "This is a welcoming screen for the entry of the application , you can now see the options"
         }
       />
+      <div className="mx-2">
+        <FillRecipesHeader action={"Fill"} title={"Fill"} />
+      </div>
     </div>
   );
 };
--- src/modules/recipes/components/RecipeForm/RecipeForm.jsx
+++ src/modules/recipes/components/RecipeForm/RecipeForm.jsx
@@ -13,6 +13,7 @@ import UploadImgBox from "../../../shared/components/UploadImgBox/UploadImgBox";
 import ShowUploadImgBox from "../../../shared/components/ShowUploadImgBox/ShowUploadImgBox";
 import useCategories from "../../../categories/components/hooks/useCategories";
 import useTags from "../../../tags/components/hooks/useTags";
+import FillRecipesHeader from "../../../shared/components/FillRecipesHeader/FillRecipesHeader";
 
 const RecipeForm = () => {
   const [hasChanges, setHasChanges] = useState(false);
@@ -152,35 +153,7 @@ const RecipeForm = () => {
 
   return (
     <div className="raw mx-3">
-      <header className="recipe-header d-flex justify-content-between  p-3 p-md-5">
-        <div className="d-flex flex-column">
-          <h4>
-            Fill the <span className="text-success ">Recipes !</span>{" "}
-          </h4>
-          <span>
-            you can now fill the meals easily using the table and form , click
-            here and sill it with the table !
-          </span>
-        </div>
-        <Link
-          to={"/recipes"}
-          className="btn btn-success fw-bold d-flex gap-2 align-items-center ms-3"
-        >
-          All Recipes
-          <svg
-            width="22"
-            height="22"
-            viewBox="0 0 22 22"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M20.9927 10.7075C20.9927 11.0168 20.8783 11.2827 20.6494 11.5054L14.5542 17.5913C14.4367 17.7088 14.313 17.7954 14.1831 17.8511C14.0532 17.9067 13.9202 17.9346 13.7842 17.9346C13.4749 17.9346 13.2214 17.8356 13.0234 17.6377C12.8255 17.446 12.7266 17.2048 12.7266 16.9141C12.7266 16.7656 12.7575 16.6265 12.8193 16.4966C12.875 16.3667 12.9523 16.2523 13.0513 16.1533L15.1294 14.0566L18.5156 10.9487L18.8867 11.5889L15.6118 11.7837H4.46045C4.13883 11.7837 3.87907 11.6847 3.68115 11.4868C3.47705 11.2889 3.375 11.0291 3.375 10.7075C3.375 10.3921 3.47705 10.1354 3.68115 9.9375C3.87907 9.73958 4.13883 9.64063 4.46045 9.64063L15.6118 9.64062L18.8867 9.83545L18.5156 10.4663L15.1294 7.36768L13.0513 5.271C12.9523 5.17204 12.875 5.05762 12.8193 4.92773C12.7575 4.79785 12.7266 4.65869 12.7266 4.51025C12.7266 4.21956 12.8255 3.97835 13.0234 3.78662C13.2214 3.5887 13.4749 3.48975 13.7842 3.48975C14.0625 3.48975 14.3161 3.60107 14.5449 3.82373L20.6494 9.91895C20.8783 10.1354 20.9927 10.3983 20.9927 10.7075Z"
-              fill="white"
-            />
-          </svg>
-        </Link>
-      </header>
+      <FillRecipesHeader action={newRecipe ? "Fill" : "Edit"} title={"All"} />
       <form
         className=" col-md-12 recipe-form "
         onSubmit={handleSubmit(onSubmit)}
