992b15183d92e36c408f620fde3bcd0238e84fdf
Your Name <ranamostafa2210@gmail.com>
2024-11-24 23:19:45 +0200

    add image path

--- src/modules/shared/components/Navbar/Navbar.jsx
+++ src/modules/shared/components/Navbar/Navbar.jsx
@@ -2,16 +2,21 @@
 import { useContext } from "react";
 import avatar from "../../../../assets/avatar.png";
 import { AuthContext } from "../../../../context/AuthContext";
+import { IMAGE_URL } from "../../../../services/api/apiConfig";
 
 const Navbar = () => {
   const { loginData } = useContext(AuthContext);
-  console.log(loginData);
+  console.log(loginData?.imagePath);
   return (
     <div
       className="py-2 px-3 d-flex justify-content-end align-items-center gap-1 my-3 mx-3 rounded"
       style={{ backgroundColor: "#F8F9FB" }}
     >
-      <img src={avatar} alt="user avatar" className="avatar" />
+      <img
+        src={loginData?.imagePath ? IMAGE_URL + loginData?.imagePath : avatar}
+        alt="user avatar"
+        className="avatar"
+      />
       <span>{loginData?.userName}</span>
     </div>
   );
